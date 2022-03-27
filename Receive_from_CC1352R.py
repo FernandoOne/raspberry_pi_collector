f9f1e5a3e037132e5f710ede365c1c6304f3bdf0
FernandoOne <fernando.mgzxc@gmail.com>
2022-03-27 17:10:55 -0300

    Changes in files

--- Receive_from_CC1352R.py
+++ Receive_from_CC1352R.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from Convert_to_JSON import *
+import json
 
 class receiveData:
     def __init__(self):
@@ -27,3 +27,22 @@ class receiveData:
             #Append it to the list of sensor data
             sensorsData.append(sensorData)
         return sensorsData
+
+#This function is used to convert the data obtained from the sensor to JSON format
+def convertSensorDataStringToJSON(dataString):
+
+    dataList = dataString.split("=")
+    for i in range(0, len(dataList)):
+        dataList[i] = dataList[i].split(",")
+
+    dataDictionary = {}
+    dataDictionary["Address"] = dataList[1][0]
+    dataDictionary["Temperature"] = dataList[2][0]
+    dataDictionary["Humidity"] = dataList[3][0]
+    dataDictionary["Light"] = dataList[4][0]
+    dataDictionary["RSSI"] = dataList[5][0]
+    dataDictionary["Time"] = dataList[6][0]
+
+    dataJSON = json.dumps(dataDictionary, indent = 4)
+
+    return dataJSON
