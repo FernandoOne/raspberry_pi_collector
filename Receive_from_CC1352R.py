67eae8d13237a259541d0faf95d51f06700869e8
FernandoOne <fernando.mgzxc@gmail.com>
2022-06-05 14:51:15 -0300

    Changed convertSensorDataStringToJSON() into class

--- Receive_from_CC1352R.py
+++ Receive_from_CC1352R.py
@@ -24,26 +24,26 @@ class receiveData:
             sensorData = sensorData + ", Time=" + datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
             #print(sensorData)
             #Convert data to JSON
-            sensorData = convertSensorDataStringToJSON(sensorData)
+            sensorData = self.convertSensorDataStringToJSON(sensorData)
             #Append it to the list of sensor data
             sensorsData.append(sensorData)
         return sensorsData
 
-#This function is used to convert the data obtained from the sensor to JSON format
-def convertSensorDataStringToJSON(dataString):
+    #This function is used to convert the data obtained from the sensor to JSON format
+    def convertSensorDataStringToJSON(dataString):
 
-    dataList = dataString.split("=")
-    for i in range(0, len(dataList)):
-        dataList[i] = dataList[i].split(",")
+        dataList = dataString.split("=")
+        for i in range(0, len(dataList)):
+            dataList[i] = dataList[i].split(",")
 
-    dataDictionary = {}
-    dataDictionary["Address"] = dataList[1][0]
-    dataDictionary["Humidity"] = dataList[3][0]
-    dataDictionary["Battery"] = dataList[4][0]
-    dataDictionary["Report_interval"] = dataList[5][0]
-    dataDictionary["RSSI"] = dataList[6][0]
-    dataDictionary["Time"] = dataList[7][0]
+        dataDictionary = {}
+        dataDictionary["Address"] = dataList[1][0]
+        dataDictionary["Humidity"] = dataList[3][0]
+        dataDictionary["Battery"] = dataList[4][0]
+        dataDictionary["Report_interval"] = dataList[5][0]
+        dataDictionary["RSSI"] = dataList[6][0]
+        dataDictionary["Time"] = dataList[7][0]
 
-    dataJSON = json.dumps(dataDictionary, indent = 4)
+        dataJSON = json.dumps(dataDictionary, indent = 4)
 
-    return dataJSON
+        return dataJSON
